a471c9cd3e0ceae2f4ad56f98384518e2ba76e30
rjowell20007 <44178512+rjowell@users.noreply.github.com>
2021-09-16 18:21:37 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,18 @@
+print("Welcome to Kai's Italian Bistro!")
+print("Please Choose Your Dish")
+
+customer_order = input("Z - Fettucini Alfredo   X - Spaghetti  Y - Lasagna   W - Pizza ")
+
+if customer_order == "Z":
+  print("Here is your Fettucini")
+elif customer_order == "Y":
+  print("Enjoy Your Spaghetti")
+elif customer_order == "X":
+  print("Here is your Lasagna")
+elif customer_order == "W":
+  print("Here is your Pizza")
+else:
+  print("We don't have that, please try again!")
+
+
+
