7e5b02bf57126fec48766a577447602b408b0efb
rjowell20007 <44178512+rjowell@users.noreply.github.com>
2021-09-21 18:23:47 +0000

    Car Code for 9/21

--- main.py
+++ main.py
@@ -1,10 +1,27 @@
 class Car:
 
+  maxFuel = 34
+  currentFuel = 8
+  lowFuel = 3
+
   def drive(self):
-    print("Zoom Zoom Zoom")
+    if self.currentFuel == 0:
+      print("You can't drive, you need to refuel!")
+    else:
+      if self.currentFuel <= self.lowFuel:
+        print("Your fuel is getting low!")
+      self.currentFuel -= 1
+      print("Zoom Zoom Zoom")
+      print(self.currentFuel)
 
   def reverse(self):
-    print("Backing Up!")
+    if self.currentFuel == 0:
+      print("You can't drive, you need to refuel!")
+    else:
+      if self.currentFuel <= self.lowFuel:
+        print("Your fuel is getting low!")
+      self.currentFuel -= 1
+      print("Backing Up!")
 
   #Constructor Method - Parameters
   def __init__(self,whatColor,makeInput,howManyPassengers,hasRims,hasSpoilers):
@@ -17,17 +34,39 @@ class Car:
   def turn(self):
     print("We're turning")
 
+  def howMuchFuelDoIHave(self):
+    return self.currentFuel
+
+  def refuel(self,howMuchFuel):
+    if self.currentFuel + howMuchFuel > self.maxFuel:
+      return False
+    else:
+      self.currentFuel += howMuchFuel
+      return True
+
+
   def honk_horn(self):
     print("Honk Honk")
 
+  def willItStart(self):
+    if self.currentFuel >= 2:
+      return True
+    else:
+      return False
+
 colesCar = Car("blue","Dodge",4,True,False)
 
-print(colesCar.color)
-print(colesCar.rims)
-
-colesCar.honk_horn()
 colesCar.drive()
-colesCar.turn()
+colesCar.drive()
+colesCar.drive()
+print(colesCar.howMuchFuelDoIHave())
+colesCar.drive()
+colesCar.drive()
+colesCar.drive()
+colesCar.refuel(20)
+colesCar.drive()
+print(colesCar.howMuchFuelDoIHave())
+
 
 '''
 
